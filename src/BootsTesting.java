1961b249ab940a6e839a75f734810f17a5d6b969
eman faisal emair <emanimair2@gmail.com>
2025-06-03 23:06:13 +0300

    test 4

--- src/BootsTesting.java
+++ src/BootsTesting.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -6,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -24,9 +26,43 @@ public class BootsTesting {
 		driver.findElement(By.id("onetrust-accept-btn-handler")).click(); 
 	}
 	
+	@Test(priority= 1 , enabled = true) 
+	public void Regisstration() throws InterruptedException {
+		WebElement RegistrationLink = driver.findElement(By.id("signInQuickLink")); 
+		RegistrationLink.click(); 
+		driver.findElement(By.xpath("//input[@value='Register']")).click(); 
+		
+		Thread.sleep(2000); 
+		//title 
+		WebElement TitleSelect = driver.findElement(By.xpath("//select[@data-display-name='Title']"));
+		TitleSelect.click(); 
+		Select TitleSelectList = new Select(TitleSelect); 
+		Thread.sleep(2000); 
 
+		TitleSelectList.selectByVisibleText("Miss");
+		
+		//first name 
+		WebElement FirstNameInput = driver.findElement(By.xpath("//input[@data-display-name='First name']")); 
+		FirstNameInput.sendKeys("Eman"); 
+		
+		//Last Name 
+		
+		WebElement LastNameInput = driver.findElement(By.xpath("//input[@data-display-name='Last name']"));
+		LastNameInput.sendKeys("Emair"); 
+		
+		//Email 
+		WebElement EmailInput =driver.findElement(By.xpath("//input[@data-display-name='Email address']")); 
+		EmailInput.sendKeys("emanimair2@gmail.com"); 
+		//Password 
+		
+		List<WebElement> Passwords = driver.findElements(By.xpath("//input[@type='password']")); 
+		for (int i  = 0 ; i < Passwords.size() ;i++) {
+			Passwords.get(i).sendKeys("emanemair@1234"); 
+		}
+		
+	}
 	
-	@Test(priority = 1 , enabled = true)
+	@Test(priority = 2 , enabled = false) 
 	public void verifyProductListingPageLoad() throws InterruptedException {
 		Thread.sleep(2000);
 
@@ -69,8 +105,10 @@ public class BootsTesting {
 	}
 	
 	
-	@Test (priority = 2 , enabled = true )
-	public void VerifiyProductListIsLoaded() {
+	@Test (priority = 3 , enabled = false)
+	public void VerifiyProductListIsLoaded() throws InterruptedException {
+		
+		Thread.sleep(2000); 
 		
 		String ProductShowenText =driver.findElement(By.cssSelector(".oct-listers__pagination.oct-listers__pagination-top.oct-listers__pagination-top__no-chanel")).getText(); 
 		Boolean ExpectedResult = true; 
@@ -80,15 +118,43 @@ public class BootsTesting {
 	
 	}
 	
-	@Test (priority = 3 , enabled = true) 
-	public void ViewingProductDetailes() {
+	@Test (priority = 4 , enabled = false)
+	public void ViewingProductDetailes() throws InterruptedException {
+		
 		
 		List<WebElement> ProductList = driver.findElement(By.id("hits")).findElements(By.cssSelector(".oct-grid__cell.oct-grid__cell--width-3.oct-grid__cell--small-6.oct-grid__cell--medium-small-4.oct-grid__cell--medium-4.oct-grid__cell--medium-large-4.oct-listers-hits__item.hit.oct-listers__item--with-inGrid\r\n"
 				+ ""));
 		int RandomProductIndex = rand.nextInt(ProductList.size()); 
-		ProductList.get(RandomProductIndex).findElement(By.tagName("a")).click(); 
+		ProductList.get(RandomProductIndex).findElement(By.tagName("a")).click();
+		Thread.sleep(2000); 
+
+		WebElement ProductDetailes = driver.findElement(By.id("estore_pdp_trcol_1")); 
+        List<Boolean> boolList = new ArrayList<>();
+        WebElement ProductName =  ProductDetailes.findElement(By.tagName("h1")); 
+        WebElement ProductPrice = ProductDetailes.findElement(By.id("PDP_productPrice")); 
+        WebElement ProductReviews = ProductDetailes.findElement(By.cssSelector(".bv_main_container_row_flex.bv_ratings_summary.bv_main_rating_button")); 
+        boolList.add(ProductName.isDisplayed()); 
+        boolList.add(ProductPrice.isDisplayed()) ; 
+        boolList.add(ProductReviews.isDisplayed()); 
+        int DispalyingCount = 0 ; 
+        for (int i = 0 ; i < boolList.size() ; i++ ) {
+        	if(boolList.get(i)!= true ) 
+        		break; 
+        	else 
+        		DispalyingCount = DispalyingCount + 1 ; 
+        }
+        
+       Boolean ExpectedResult = true; 
+       Boolean ActualResult = false; 
+       if (DispalyingCount == 3) {
+    	   ActualResult = true; 
+       }
+		
+       Assert.assertEquals(ActualResult, ExpectedResult); 
 
 	}
+	
+	
 	
 
 	
